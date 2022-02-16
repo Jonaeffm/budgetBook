c5e2a3479274c412a417b7de3377cbcc6a39b9b8
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2022-02-16 19:02:56 +0100

    Add files via upload

---
+++ src/main/java/budgetBook/budget.java
@@ -0,0 +1,49 @@
+package budgetBook;
+
+import java.sql.Date;
+
+public class budget {
+	private int id;
+	private Date date;
+	private String product;
+	private Double price;
+	
+	public budget(Date d,String p, Double pr)
+	{
+		setDate(d);
+		setProduct(p);
+		setPrice(pr);
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
